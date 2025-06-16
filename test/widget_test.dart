997700e00541c133b59cf346213a563254de15c3
apit522 <118596078+apit522@users.noreply.github.com>
2025-06-16 18:16:47 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:smart_energy_ac/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyApp(autoLogin: false));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
